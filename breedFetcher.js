fcd213154ee32e32acbfefa16ad5da56dac7aa39
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-30 16:06:25 -0600

    Add index.js for refractoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,26 +3,32 @@
  * Users can provide any breed name, causing the application to fetch the information from the API and print out a short description of that breed.
  */
 const request = require('request');
-let args = process.argv.slice(2);
 const urlOfWebsite = "https://api.thecatapi.com/v1/breeds/search?q=";
-let breedName = args[0];
-if (!breedName) {
-  console.log("Please enter a breed name."); //if no command line argument is passed
-  return;
-}
-request(`${urlOfWebsite}${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log(`Error occurred while fetching the data:, ${error}`);
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  if (!breedName) {
+    console.log("Please enter a breed name."); //if no command line argument is passed
     return;
   }
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);
-  if (data.length === 0) { //if breedname is not found
-    console.log("Breed not found");
-    return;
-  }
-  console.log(data[0].description); //getting first element of data array and printing the description
-});
+  request(`${urlOfWebsite}${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null); //description will be null, if there is an error
+      return;
+    }
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    const data = JSON.parse(body);
+    if (data.length === 0) { //if breedname is not found
+      callback("Breed not found");
+      return;
+    }
+    callback(null, data[0].description); //getting first element of data array and printing the description and error argument will be null.
+  });
+};
+
+
+
+module.exports = fetchBreedDescription;
    
 
 
